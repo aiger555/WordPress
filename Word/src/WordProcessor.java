55c1a5cb9712afa3af6ed249b9608b8931eff5c1
Aigerim Nuralieva <75246512+aiger555@users.noreply.github.com>
2023-11-18 21:25:53 +0600

    Add files via upload

---
+++ Word/src/WordProcessor.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+class WordProcessor {
+    private Set<String> distinctWords;
+
+    WordProcessor() {
+        distinctWords = new HashSet<>();
+    }
+
+    public void readFile(String filePath) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Split the line into words and add them to the set
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    distinctWords.add(word.toLowerCase()); // Convert to lowercase for case-insensitive comparison
+                }
+            }
+        }
+    }
+
+    public Set<String> getDistinctWords() {
+        return distinctWords;
+    }
+}
